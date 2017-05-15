85c9d6d80aa2e6977bc9afa3c309e8652deb07b4
zhaoshiwei1 <447518646@qq.com>
2017-05-15 11:31:14 +0800

    auto save! some issues in the post action

--- db_action.py
+++ db_action.py
@@ -8,10 +8,17 @@ class db_action:
         result_set = self.db.cu.fetchall()
         return  result_set
     def get_api_list_by_module_id(self, id):
-        rec = self.db.cu.execute("SELECT API_NAME, API_CMT, IF_VALID FROM common WHERE MODEL_ID =" + "\'"+ id + "\'")
+        rec = self.db.cu.execute("SELECT ID, API_NAME, API_CMT, IF_VALID FROM common WHERE MODEL_ID =" + "\'"+ id + "\'")
         result_set = self.db.cu.fetchall()
         return result_set
     def get_api_list_title(self, id):
-        rec = self.db.cu.execute("SELECT API_NAME, API_CMT, IF_VALID FROM common WHERE MODEL_ID =" + "\'"+ id + "\'")
+        rec = self.db.cu.execute("SELECT ID, API_NAME, API_CMT, IF_VALID FROM common WHERE MODEL_ID =" + "\'"+ id + "\'")
         col_name_list = [tuple[0] for tuple in rec.description]
         return col_name_list
+    def change_api_status(self, id):
+        rec = self.db.cu.execute("SELECT API_NAME, API_CMT, IF_VALID FROM common WHERE ID =" + "\'"+ id + "\'")
+        result_set = self.db.cu.fetchall()
+        if result_set[0][2] == "YES":
+            self.db.cu.execute("UPDATE common SET IF_VALID ='NO' WHERE ID =" + "\'"+ id + "\'")
+        elif result_set[0][2] == "NO":
+            self.db.cu.execute("UPDATE common SET IF_VALID ='YES' WHERE ID =" + "\'"+ id + "\'")
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ sys.setdefaultencoding('utf8')
 
 urls = (
     '/', 'index',
-    '/show_apis', 'show_api_list'
+    '/show_apis', 'show_api_list',
+    '/change_status', 'change_status'
 )
 
 class index:
@@ -84,17 +85,22 @@ class show_api_list:
             show_default_filted = show_default_filted + """<th align="left">"""
             show_default_filted = show_default_filted + name
             show_default_filted = show_default_filted + """</th>"""
-        show_default_filted +="</tr>"
+        show_default_filted +="""<th>Action</th></tr>"""
         for ret in result_set:
-            show_default_filted = show_default_filted + """<tr><td align = "left">"""
-            show_default_filted = show_default_filted + ret[0]
+            show_default_filted = show_default_filted + """<tr>"""
+            show_default_filted = show_default_filted + """<form action = "/change_status" method = "post"><td align = "left"><input type = "text" name = "id" value ="""+str(ret[0]) + """ disabled="true" >"""
             show_default_filted = show_default_filted + """</td>"""
             show_default_filted = show_default_filted + """<td align = "left">"""
             show_default_filted = show_default_filted + ret[1]
             show_default_filted = show_default_filted + """</td>"""
             show_default_filted = show_default_filted + """<td align = "left">"""
             show_default_filted = show_default_filted + ret[2]
-            show_default_filted = show_default_filted + """</td></tr>"""
+            show_default_filted = show_default_filted + """</td>"""
+            show_default_filted = show_default_filted + """<td align = "left">"""
+            show_default_filted = show_default_filted + ret[3]
+            show_default_filted = show_default_filted + """</td>"""
+            show_default_filted = show_default_filted + """<td><button type = "submit">更改接口有效性</button></td></form></tr>"""
+
         show_default_filted += """</table>"""
         return header + filter +show_default_filted + end
 
@@ -141,21 +147,33 @@ class show_api_list:
             show_default_filted = show_default_filted + name
             show_default_filted = show_default_filted + """</th>"""
 
-        show_default_filted +="</tr>"
+        show_default_filted +="""<th>Action</th></tr>"""
 
         for ret in result_set:
-            show_default_filted = show_default_filted + """<tr><td align = "left">"""
-            show_default_filted = show_default_filted + ret[0]
+            show_default_filted = show_default_filted + """<tr><form action = "/change_status" method = "post">"""
+            show_default_filted = show_default_filted + """<td align = "left"><input type = "text" name = "id" value ="""+str(ret[0]) + """ disabled="true" >"""
             show_default_filted = show_default_filted + """</td>"""
             show_default_filted = show_default_filted + """<td align = "left">"""
             show_default_filted = show_default_filted + ret[1]
             show_default_filted = show_default_filted + """</td>"""
             show_default_filted = show_default_filted + """<td align = "left">"""
             show_default_filted = show_default_filted + ret[2]
-            show_default_filted = show_default_filted + """</td></tr>"""
+            show_default_filted = show_default_filted + """</td>"""
+            show_default_filted = show_default_filted + """<td align = "left">"""
+            show_default_filted = show_default_filted + ret[3]
+            show_default_filted = show_default_filted + """</td>"""
+            show_default_filted = show_default_filted + """<td><button type = "submit">更改接口有效性</button></td></form></tr>"""
 
         show_default_filted += """</table>"""
         return header + filter +show_default_filted + end
+
+class change_status:
+    def POST(self):
+        d_a = db_action()
+        i = web.input('id')
+        print i.id
+        d_a.change_api_status(self, i.id)
+        return web.seeother('/show_apis')
 
 if __name__=="__main__":
     app = web.application(urls,globals())
