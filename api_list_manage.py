283ea5e8face9d0948b037a75ed227862a96f7cd
shiwei <447518646@qq.com>
2017-05-16 23:13:01 +0800

    hid the input

--- api_list_manage.py
+++ api_list_manage.py
@@ -81,8 +81,8 @@ def post_selected_api_list_html_string(api_result_list):
     result_string = """
     """
     for result in api_result_list:
-        result_string += """<tr><td align = "right">"""+ result[0]+"""</td><td align = "right">"""+result[1]+"""</td><td align = "right">"""+result[2]+"""</td><td align = "right">"""+result[3]+"""</td>"""
-        result_string += """<td><form action = "/change_status" method = "post"><input type = "text" name = "r_id" value ="""+ "\""+ result[0]+"\""+"""><button type = "submit">修改</form></td></tr>"""
+        result_string += """<tr><td align = "left">"""+ result[0]+"""</td><td align = "left">"""+result[1]+"""</td><td align = "left">"""+result[2]+"""</td><td align = "left">"""+result[3]+"""</td>"""
+        result_string += """<td><form action = "/change_status" method = "post"><input type = "text" name = "r_id" value ="""+ "\""+ result[0]+"\""+""" style="display:none"><button type = "submit">修改</form></td></tr>"""
     result_string += """</table>"""
 
     end = """
