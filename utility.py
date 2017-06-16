79b6cf190653c588d4f3e805d2b0ae3b1197a1b2
zhaoshiwei1 <447518646@qq.com>
2017-06-16 17:54:46 +0800

    format some files

--- utility.py
+++ utility.py
@@ -4,17 +4,21 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
 def make_id_key(max_id):
     return str(max_id+1)
 
+
 def make_module_id(max_specific_module_id):
-    if(max_specific_module_id<9):
-        return  "0" + str(max_specific_module_id+1)
+    if max_specific_module_id<9:
+        return "0" + str(max_specific_module_id+1)
     else:
         return str(max_specific_module_id+1)
 
+
 def make_table_name(module_name, specific_module_id):
     return module_name + specific_module_id
+
 
 def make_module_name(module_id):
     values = {
@@ -28,19 +32,21 @@ def make_module_name(module_id):
     return values[module_id]
 
 
-def make_insert_sql(module_id,module_name, api_name, http_method, url, cmt, wanted_id, wanted_max_specific_module_id, wanted_table_name):
+def make_insert_sql(module_id, module_name, api_name, http_method, url, cmt, wanted_id, wanted_max_specific_module_id,
+                    wanted_table_name):
     sql_string = """INSERT INTO general VALUES("""
-    sql_string += ("\'"+ wanted_id +"\',")
-    sql_string += ("\'"+ module_id +"\',")
-    sql_string += ("\'"+ module_name +"\',")
-    sql_string += ("\'"+ wanted_max_specific_module_id +"\',")
-    sql_string += ("\'"+ api_name +"\',")
-    sql_string += ("\'"+ cmt +"\',")
-    sql_string += ("\'"+ wanted_table_name +"\',")
-    sql_string += ("\'"+ http_method +"\',")
-    sql_string += ("\'"+ url +"\',")
+    sql_string += ("\'" + wanted_id + "\',")
+    sql_string += ("\'" + module_id + "\',")
+    sql_string += ("\'" + module_name + "\',")
+    sql_string += ("\'" + wanted_max_specific_module_id + "\',")
+    sql_string += ("\'" + api_name + "\',")
+    sql_string += ("\'" + cmt + "\',")
+    sql_string += ("\'" + wanted_table_name + "\',")
+    sql_string += ("\'" + http_method + "\',")
+    sql_string += ("\'" + url + "\',")
     sql_string += """'YES') """
     return sql_string
+
 
 def make_create_table_sql(table_name, parameters_string):
     create_table_string = """"""
@@ -49,8 +55,9 @@ def make_create_table_sql(table_name, parameters_string):
     create_table_string += """(ID TEXT NOT NULL,"""
     for parameter in parameter_list:
         create_table_string += parameter + """  TEXT NOT NULL,"""
-    create_table_string +="""PRIMARY KEY (ID))"""
+    create_table_string += """PRIMARY KEY (ID))"""
     return create_table_string
+
 
 def make_insert_sql_by_selected_api_id(tb_name, wanted_id, parameter_name_list, parameter_value_list):
     sql_string = """INSERT INTO """ + tb_name + """ ( ID, """
@@ -65,7 +72,7 @@ def make_insert_sql_by_selected_api_id(tb_name, wanted_id, parameter_name_list, 
                 sql_string += p_name + """ ,"""
             else:
                 sql_string += p_name + """) """
-        sql_string += """VALUES (""" +"\'"+wanted_id+"\', "
+        sql_string += """VALUES (""" + "\'" +wanted_id+"\', "
         for p_value in parameter_value_list:
             j += 1
             if j < value_length:
@@ -75,6 +82,7 @@ def make_insert_sql_by_selected_api_id(tb_name, wanted_id, parameter_name_list, 
         return sql_string
     else:
         return "PARAMETER ERROR"
+
 
 def make_update_sql(tb_name, case_id, parameter_name_list, parameter_value_list):
     sql_string = """UPDATE """ + tb_name + """ SET """
